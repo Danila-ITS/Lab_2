6f010c4b03318a874681ff911ead66684173aa1d
Danila Zakharov <50679329+Danila-ITS@users.noreply.github.com>
2021-02-06 08:22:10 +0300

    Update Individual.py

--- Individual.py
+++ Individual.py
@@ -6,13 +6,13 @@
 
 num_1 = int(input("Введите число 1: "))
 num_2 = int(input("Введите число 2: "))
- a = num_1
- b = num_2
- c = a + b
- d = a - b
- e = a * b
- f = a / b
- print("Сумма чисел: ", c)
- print("Разность чисел: ", d)
+    a = num_1
+    b = num_2
+    c = a + b
+    d = a - b
+    e = a * b
+    f = a / b
+print("Сумма чисел: ", c)
+print("Разность чисел: ", d)
 print("Произведение чисел: ", e)
 print("Деление чисел: ", f)
